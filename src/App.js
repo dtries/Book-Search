c976cfb15d577a2130f87a402e5b33f7150e28b4
Dennis Ries <dtries@gmail.com>
2019-04-12 13:14:25 -0600

    Some initial formattting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        
+        <nav>
+          <div className="nav-wrapper">
+            <a href="/" className="left">Book Search</a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li><a href="search.html">Search</a></li>
+              <li><a href="saved.html">Saved</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="App-header">
+          <h2>Book Search
+          </h2>
+        </div>
+
+        <div className="search-area">
+          <h2>Search For a Book
+          </h2>
+
+          <div class="input-field col s6">
+            <input value="Enter a Book Title Here" id="first_name2" type="text" class="validate"/>
+            <label class="active" for="first_name2">Book Title</label>
+          </div>
+
+
+
+
+          
+        </div>
+
+
       </div>
     );
   }
